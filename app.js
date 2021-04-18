7f9318395bf2c591e6168dcc3f8e94f4e2b97f47
JC <jcbrady89@yahoo.com>
2021-04-17 22:29:33 -0600

    Initial content

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const app = express();
 const pug = require("pug");
+const path = require("path");
 
 // view engine pug
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
-//app.set("views", path.join(__dirname, "views"));
+
+app.use("/static", express.static("public"));
 
 app.get("/", (req, res) => {
   res.render("index");
   //res.send("Hello world.");
+});
+
+app.get("/about", (req, res) => {
+  res.render("./about");
 });
 
 // PORT - Reference: Code with Mosh demo
